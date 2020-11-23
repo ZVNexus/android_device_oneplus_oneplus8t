7bf9ff4b975bc8afb3112de48fb53690b1949027
Alexander Koskovich <zvnexus@outlook.com>
2020-11-23 06:12:14 -0700

    OnePlus 8T.
     * An extension of the primary tree for the OnePlus 8.
    
    Change-Id: I64d47593ecb8f3344ffa8522ef7b6df6a13646c8

---
+++ device.mk
@@ -0,0 +1,22 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/oneplus/oneplus8/device.mk)
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/oneplus/oneplus8t
+
+# Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
