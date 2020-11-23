7442b2c5c233e59d12571f04c276e163117af763
Alexander Koskovich <zvnexus@outlook.com>
2020-11-23 08:39:26 -0700

    op8t: Include support for the GC02M1B camera sensor.
     * OP8 camera stack has references to this, should
       be OK to include it.
    
    Change-Id: I5e85229a40f7390f1ce90f34d3d1e9b9cb93b6d1

--- device.mk
+++ device.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, vendor/oneplus/oneplus8t/oneplus8t-vendor.mk)
 $(call inherit-product, device/oneplus/oneplus8/device.mk)
 
 # Namespaces
